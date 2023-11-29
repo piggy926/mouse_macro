0e539f7f570795f775c325769d56075bad7c6578
piggy <208455520@qq.com>
2023-11-29 11:28:46 +0800

    配置描述更新

--- src/main/java/com/piggy/config/MacroConfig.java
+++ src/main/java/com/piggy/config/MacroConfig.java
@@ -3,14 +3,18 @@ package com.piggy.config;
 import com.piggy.pojo.WeaponData;
 import com.piggy.pojo.WeaponRoot;
 
+/**
+ * 宏全局参数
+ */
 public class MacroConfig {
 
     // 全局武器配置
-    public static WeaponRoot weaponRoot;
+    public static WeaponRoot weaponRoot = null;
     // 当前武器配置
-    public static WeaponData currentWeapon;
+    public static WeaponData currentWeapon = null;
     // 鼠标左键是否按下
     public static boolean running = false;
     // ctrl是否按下
     public static boolean ctrlIsPress = false;
+
 }
